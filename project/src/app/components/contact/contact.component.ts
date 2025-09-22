5e9f17315bea31e98a4c28ad5625b66b7763c85d
ridhimasapre-pageupsoft <ridhimasapre74@gmail.com>
2025-09-22 21:53:04 +0530

    update my portfolio

--- project/src/app/components/contact/contact.component.ts
+++ project/src/app/components/contact/contact.component.ts
@@ -1,10 +1,12 @@
+import emailjs from 'emailjs-com';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'] 
 })
@@ -14,9 +16,33 @@ export class ContactComponent {
     email: '',
     message: ''
   };
+  status :String = ''
 
   onSubmit() {
     console.log('Form submitted:', this.formData);
     // Add your form submission logic here
+
+    if (!this.formData.name || !this.formData.email || !this.formData.message) {
+      this.status = 'Please fill in all fields!';
+      return;
+    }
+
+    const templateParams = {
+      name: this.formData.name,
+      email: this.formData.email,
+      message: this.formData.message
+    };
+
+    // Email.js configuration
+    emailjs.send('service_64pysnc', 'template_62m3wlo', templateParams, 'EXgM2poGjR-o3godH')
+      .then((result) => {
+        this.status = 'Email sent successfully!';
+        console.log(result.text);
+      }, (error) => {
+        this.status = 'Error sending email. Please try again.';
+        console.log(error.text);
+      });
   }
+
+  
 }
